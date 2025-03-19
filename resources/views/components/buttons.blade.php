c85dbae3dda5712c09d47c686a73a9a9990c4bd3
JohnnySeb <jean-sebastien@agencetolle.com>
2025-03-19 17:40:10 -0400

    Add icon support to button component with dynamic rendering

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -1,7 +1,25 @@
-
 @if (! empty($buttonsClone['buttons']))
     <div class="btn-wrapper insight ghost {{ $extraClasses ?? '' }}">
         @foreach ($buttonsClone['buttons'] as $button)
+            @php
+                $icon = $button['icon'] ?? '';
+
+                if (!empty($icon)) {
+                    $icon_id = $icon['id'];
+            
+                    $icon = wp_get_attachment_image(
+                        $icon_id,
+                        'full',
+                        false,
+                        [
+                            'class' => 'w-[15px] h-[15px]',
+                            'loading' => 'lazy',
+                            'srcset' => wp_get_attachment_image_srcset($icon_id),
+                        ]
+                    );
+                }
+            @endphp
+
             <a
                 href="{{ $button['link']['url'] ?? '' }}"
                 target="{{ $button['link']['target'] ?? '_self' }}"
@@ -9,7 +27,7 @@
                 class="group btn {{ $button['variant'] ? '--' . $button['variant'] : '' }}"
             >
                 @if (! empty($button['icon']) && $button['icon_position'] == 'before')
-                    <i class="{{ $button['icon'] ?? '' }}"></i>
+                    {!! $icon !!}
                 @endif
         
                 @if (! empty($button['link']['title']))
@@ -19,7 +37,7 @@
                 @endif
 
                 @if (! empty($button['icon']) && $button['icon_position'] == 'after')
-                    <i class="{{ $button['icon'] ?? '' }}"></i>
+                    {!! $icon !!}
                 @endif
             </a>
         @endforeach
