3586dd081ebe941430dcc7f0485554775cbf9746
JohnnySeb <jean-sebastien@agencetolle.com>
2026-03-03 16:37:10 -0500

    update: refine instructions in intro template and enhance formatting for clarity

--- resources/views/partials/intro.blade.php
+++ resources/views/partials/intro.blade.php
@@ -1,6 +1,6 @@
 <div class="fixed top-0 left-0 w-[100vw] h-[100vh] flex flex-col justify-center items-center bg-gray-50 p-4 z-[420]">
-    <div class="border border-green-400 bg-white rounded-xl p-10 max-w-[750px] shadow-md">
-        <h1 class="font-display text-48 md:text-74 font-bold text-[pink]">
+    <div class="border border-green-400 bg-white rounded-xl py-4 px-7 max-w-[750px] shadow-md">
+        <h1 class="font-display text-48 md:text-74 font-bold text-[pink] !mb-4">
             Bravo! 🎉
         </h1>
 
@@ -9,38 +9,50 @@
         </p>
 
         <p class="text-lg text-gray-600 !m-0">
-            Voici ce qu'il te reste à faire:
+            Voici ce qu'il te reste à faire avant de plonger dans le code:
         </p>
 
-        <ol>
+        <ul>
             <li>
-                1. Update <span class="font-semibold">canIuse</span>: copier et exécuter cette commande dans le terminal:
-            </li>
-            <li class="text-sm py-2 leading-[2]">
-                <code class="bg-gray-200 p-2">npx update-browserslist-db@latest</code>
+                1. Change le "Theme Name" dans le fichier <span class="font-semibold">style.css</span> par le nom du projet.
             </li>
             <li>
-                2. Change le "Theme Name" dans le fichier <span class="font-semibold">style.css</span> par le nom du projet.
+                2. <span class="font-semibold">Supprimer</span> les plugins non utilisés.
             </li>
             <li>
-                3. Installer les plugins qui se trouvent dans le dossier <span class="font-semibold">utilities/plugins</span> à la racine du thème.
+                3. <span class="font-semibold">Installer</span> les plugins qui se trouvent dans le dossier <span class="font-semibold">utilities/plugins</span> à la racine du thème.
             </li>
             <li>
-                4. Générer les variables dans <span class="font-semibold">resources/css/_theme_variables.css</span>
+                4. <span class="font-semibold">Activer les licences</span> ACF + WPML + Gravity Forms et les <span class="font-semibold">mettre à jour</span>.
             </li>
             <li>
                 5. Synchroniser les blocs <span class="font-semibold">ACF</span>.
             </li>
             <li>
-                6. Modifier le code <span class="font-semibold">utm</span> dans le footer (référence vers Tollé).
+                6. <span class="font-semibold">Créer les pages</span> qui se trouvent dans les navigations.
             </li>
             <li>
-                7. Retirer la ligne 9 de <span class="font-semibold">index.blade.php</span> (cette intro!)
+                7. <span class="font-semibold">Créer les custom posts type</span> qui se trouvent dans le projet.
+            </li>
+            <li>
+                8. Définir Homepage et Posts page <span class="font-semibold">Settings > Reading</span>.
+            </li>
+            <li>
+                9. Définir le <span class="font-semibold">favicon</span>.
+            </li>
+            <li>
+                10. Créer les emplacement des menus dans <span class="font-semibold">setup.php</span>.
+            </li>
+            <li>
+                11. <span class="font-semibold">Régler les permaliens</span>.
+            </li>
+            <li>
+                12. Retirer la ligne 9 de <span class="font-semibold">index.blade.php</span> (cette intro!)
             </li>
         </ul>
 
-        <h2 class="font-display text-48 text-[pink] mt-10">
-            Bonne chance!
+        <h2 class="font-display text-48 text-[pink] mt-4">
+            Bon code!
         </h2>
     </div>
 </div>
