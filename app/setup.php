4d6aa10f98afe0e13ab65ea844667295ec077c5c
JohnnySeb <jean-sebastien@agencetolle.com>
2026-03-05 16:47:52 -0500

    update: add footer navigation menu and enhance footer content with logo, contact details, and links

--- app/setup.php
+++ app/setup.php
@@ -132,6 +132,7 @@ add_action('after_setup_theme', function () {
      */
     register_nav_menus([
         'primary_navigation' => __('Primary Navigation', 'tolle'),
+        'footer_navigation' => __('Footer Navigation', 'sage'),
     ]);
 
     /**
--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,6 +1,80 @@
+@php
+    $logo = get_field('logo_footer', 'options');
+    $address = get_field('address', 'options');
+    $phone = get_field('phone', 'options');
+    $email = get_field('email', 'options');
+@endphp
+
 <footer class="bg-black">
     <div class="container">
         <div class="wrap">
+            <div class="flex flex-col lg:flex-row items-center lg:items-start justify-center lg:justify-normal gap-10 lg:gap-20 2xl:gap-32 w-full">
+                {{-- LOGO --}}
+                @if ( !empty($logo) )
+                    <a
+                        href="<?php echo(function_exists('pll_home_url') ? pll_home_url() : home_url()); ?>"
+                        title="<?php echo esc_attr(get_bloginfo('name')); ?>"
+                        target="_self"
+                        class="logo xl:py-1.5 insight ghost"
+                    >
+                        @if ( !empty($logo) )
+                            <img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}" />
+                        @endif
+                    </a>
+                @endif
+
+                {{-- COORDONNÉES --}}
+                <div class="flex flex-col gap-6 max-w-60 text-center lg:text-left insight ghost">
+                    <p class="text-16 font-bold text-white uppercase">Coordonnées</p>
+
+                    @if ( !empty($address) )
+                        <a
+                            href="https://www.google.com/maps/search/?api=1&query={{ urlencode($address) }}"
+                            title="{{ $address }}"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            class="text-16 text-white hover:text-white/75 duration-300"
+                        >
+                            {{ $address }}
+                        </a>
+                    @endif
+
+                    <div class="flex flex-col gap-1">
+                        @if ( !empty($phone) )
+                            <a
+                                href="tel:{{ preg_replace('/\s+/', '', $phone) }}"
+                                title="{{ $phone }}"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                class="text-16 text-white hover:text-white/75 duration-300"
+                            >
+                                {{ $phone }}
+                            </a>
+                        @endif
+
+                        @if ( !empty($email) )
+                            <a
+                                href="mailto:{{ $email }}"
+                                title="{{ $email }}"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                class="text-16 text-white hover:text-white/75 duration-300"
+                            >
+                                {{ $email }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
+
+                {{-- MENU --}}
+                <div class="hidden xl:block insight ghost">
+                    @if ( has_nav_menu('footer_navigation') )
+                        <nav class="nav-footer" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+                            {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+                        </nav>
+                    @endif
+                </div>
+            </div>
             {{-- LEGAL --}}
             <div class="flex flex-col sm:flex-row justify-center sm:justify-between items-center gap-1.5 py-6 insight ghost">
                 <div class="flex flex-col sm:flex-row justify-center sm:justify-start gap-4 sm:gap-12">
